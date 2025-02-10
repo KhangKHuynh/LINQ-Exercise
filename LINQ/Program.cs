b161a6498c2abb407e634f9225ed31f69908fae7
Khang Kyle Huynh <khanghuynh@Khangs-MacBook-Pro.local>
2025-02-10 13:33:38 -0600

    turnin

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Dota", "AmongUs", "EldenRing" };
+            videoGames.Add("League");
+            videoGames.Add("Fortnite");
+            
+            var orderedGames= videoGames.OrderBy(name => name.Length);
+
+            foreach (var games in orderedGames)
+            {
+                Console.WriteLine(games);
+            }
+            {
+                
+            }
         }
     }
 }
